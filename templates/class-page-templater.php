fb31c9c921aafc673cde93bdb737e24d6ca60e04
tony <thang@beostore.io>
2019-09-11 10:27:53 +0700

    update 18

--- templates/class-page-templater.php
+++ templates/class-page-templater.php
@@ -140,8 +140,8 @@ class PageTemplater
         global $wpdb;
         $table_insert = $wpdb->prefix . "posts";
         $join_table = $wpdb->prefix . "postmeta";
-        $sql = "SELECT * FROM %s AS p INNER JOIN %s AS meta ON p.ID = meta.post_id WHERE post_type = '%s' AND post_status='%s' AND (meta_value = '%s' OR meta_key = '%s')";
-        $result = $wpdb->get_results($wpdb->prepare($sql, $table_insert, $join_table, 'page', 'publish', 'mstore-checkout-template.php', '_mstore_checkout_template'),OBJECT);
+        // $sql = ;
+        $result = $wpdb->get_results($wpdb->prepare("SELECT * FROM %s AS p INNER JOIN %s AS meta ON p.ID = meta.post_id WHERE post_type = '%s' AND post_status='%s' AND (meta_value = '%s' OR meta_key = '%s')", $table_insert, $join_table, 'page', 'publish', 'mstore-checkout-template.php', '_mstore_checkout_template'),OBJECT);
         if(empty($result)){
             $pageguid = site_url() . "/mstore-checkout";
             // Insert the post into the database
